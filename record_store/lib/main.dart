d8492069835ff5a85e9fa57fce19576a4c414b5b
Steven Tohme <steventohme59@gmail.com>
2024-01-16 15:37:16 -0500

    first commit

---
+++ record_store/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  );
+  runApp(MyApp());
+}
